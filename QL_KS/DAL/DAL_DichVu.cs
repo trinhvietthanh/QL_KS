5fc8e9b5c47141624920dc0a0977ceeb027a3296
Mingg2211 <minhk17khmt@gmail.com>
2021-03-24 00:38:34 +0700

    add frmQLDV

---
+++ QL_KS/DAL/DAL_DichVu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QL_KS
+{
+    public class DAL_DichVu
+    {
+        private KetNoiData connect = new KetNoiData();
+        public DataTable ThongTinDichVu(string MaDV)
+        {
+            DataTable tb = connect.GetDataTable("select * from tblDichVu where MaDV = '" + MaDV + "'");
+            return tb;
+        }
+
+        public void ThemThongTin(EC_DichVu EC_DV)
+        {
+            connect.ThucHienLenh("insert into tblDichVu(MaDV,TenDV,Gia) values(N'" + EC_DV.MaDV + "', N'" + EC_DV.TenDV + "', '" + EC_DV.Gia + "')");
+        }
+
+        public void SuaThongTin(EC_DichVu EC_DV)
+        {
+            connect.ThucHienLenh("update tblDichVu set TenDV = N'" + EC_DV.TenDV + "', Gia = N'" + EC_DV.Gia + "'  where MaDV = '" + EC_DV.MaDV + "'");
+        }
+
+        public int XoaTTDichVu(EC_DichVu EC_DV)
+        {
+            return connect.ThucHienLenh("delete from tblDichVu where MaDV = '" + EC_DV.MaDV + "'");
+        }
+    
+        public DataTable GetAll()
+        {
+            return connect.GetDataTable(@"select * from tblDichVu");
+        }
+
+        public DataTable GetAll(string dk)
+        {
+            return connect.GetDataTable(@"select * from tblDichVu where " + dk);
+        }
+
+        public string GetGia(string MaDV)
+        {
+            return connect.GetValue("Select Gia from tblDichVu where MaDV = '" + MaDV +"'");
+        }
+    }
+}
