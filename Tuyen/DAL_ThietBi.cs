99bbc8e86e381a3311e5410f37b4ce71b071a04c
tuyen1601 <tuyen160100@gmail.com>
2021-03-24 00:22:09 +0700

    update GUI

---
+++ Tuyen/DAL_ThietBi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QL_KS;
+
+namespace QL_KS
+{
+    public class DAL_ThietBi
+    {
+        private KetNoiData connect = new KetNoiData();
+
+        public int ThemThongTin(EC_ThietBi EC_ThietBi)
+        {
+            return connect.ThucHienLenh("insert into tblThietBi(MaPh, ThietBi, SoLuong, NhaSanXuat) values(N'" + EC_ThietBi.MaPh + "', N'" + EC_ThietBi.ThietBi + "', '" + EC_ThietBi.SoLuong + "', N'" + EC_ThietBi.NhaSanXuat + "') ");
+        }
+
+        public int SuaThongTin(EC_ThietBi EC_ThietBi)
+        {
+           return connect.ThucHienLenh("update tblThietBi set SoLuong='"+EC_ThietBi.SoLuong+"',NhaSanXuat=N'"+EC_ThietBi.NhaSanXuat+"' where MaPh=N'"+EC_ThietBi.MaPh+"' and ThietBi=N'"+EC_ThietBi.ThietBi+"'");
+        }
+
+        public int  XoaTTThietBi(EC_ThietBi EC_ThietBi)
+        {
+            return connect.ThucHienLenh("delete from tblThietBi where MaPh=N'" + EC_ThietBi.MaPh + "' and ThietBi=N'" + EC_ThietBi.ThietBi + "'");
+            
+        }
+        public DataTable TaoBang(string DieuKien)
+        {
+            return connect.GetDataTable(@"SELECT * FROM tblThietBi " +DieuKien);
+        }
+        public DataTable TruyVanRaMaPh(string DieuKien)
+        {
+            return connect.GetDataTable(@"SELECT MaPh from tblPhong " + DieuKien);
+        }
+    }
+}
