1d61f6c6775ccbab1cec44997a1ffc867f7e1733
hienhoang2201 <67214817+hienhoang2201@users.noreply.github.com>
2021-03-24 00:01:03 +0700

    thêm module nhân vien

---
+++ QL_KS/DAL/DAL_NhanVien.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QL_KS;
+
+namespace QL_KS
+{
+    public class DAL_NhanVien
+    {
+        private KetNoiData connect = new KetNoiData();
+        public DataTable ThongTinNV(string us)
+        {
+            DataTable tb = connect.GetDataTable(@"select * from tblNhanVien where MaNV = '" + us +"'");
+            return tb;
+        }
+
+        public void ThemThongTin(EC_NhanVien EC_NV)
+        {
+            string a = "1995-12-12";
+            connect.ThucHienLenh(@"insert into tblNhanVien(MaNV, TenNV, NgaySinh, GioiTinh, CMND, DiaChi, SoDT) values(N'" + EC_NV.MaNV + "', N'" + EC_NV.TenNV + "', '" + EC_NV.NgaySinh + "', N'" + EC_NV.GioiTinh + "', '" + EC_NV.CMND + "', N'" + EC_NV.DiaChi + "', '" + EC_NV.SoDT + "') ");
+        }
+
+        public void SuaThongTin(EC_NhanVien EC_NV)
+        {
+            string a = "1995-12-12";
+            connect.ThucHienLenh(@"update tblNhanVien set TenNV = N'" + EC_NV.TenNV + "', NgaySinh = '" + EC_NV.NgaySinh + "', GioiTinh = N'" + EC_NV.GioiTinh + "', CMND = '" + EC_NV.CMND + "', SoDT = '" + EC_NV.SoDT + "', DiaChi = N'" + EC_NV.DiaChi + "'  where MaNV = '" + EC_NV.MaNV + "'");
+        }
+
+        public void XoaTTNhanVien(EC_NhanVien EC_NV)
+        {
+            connect.ThucHienLenh(@"delete from tblNhanVien where MaNV = '" + EC_NV.MaNV + "'");
+            connect.ThucHienLenh(@"insert into tblNhanVien(MaNV) values('" + EC_NV.MaNV + "')");
+        }
+      
+    }
+}
